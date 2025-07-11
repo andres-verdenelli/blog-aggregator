8d23a2a7cdf37915a8eef7c11476374ed3aa8804
Andres Verdenelli <andresverdenelli@icloud.com>
2025-07-11 09:23:36 -0300

    Create table for feeds

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,11 +1,25 @@
 import { pgTable, timestamp, uuid, text } from 'drizzle-orm/pg-core'
 
-export const users = pgTable('users', {
-  id: uuid('id').primaryKey().defaultRandom().notNull(),
+const timestamps = {
   created_at: timestamp('created_at').notNull().defaultNow(),
   updated_at: timestamp('updated_at')
     .notNull()
     .defaultNow()
     .$onUpdate(() => new Date()),
+}
+
+export const users = pgTable('users', {
+  id: uuid('id').primaryKey().defaultRandom().notNull(),
   name: text('name').notNull().unique(),
+  ...timestamps,
+})
+
+export const feeds = pgTable('feeds', {
+  id: uuid('id').primaryKey().defaultRandom().notNull(),
+  name: text('name').notNull(),
+  url: text('url').notNull().unique(),
+  user_id: uuid('user_id')
+    .notNull()
+    .references(() => users.id, { onDelete: 'cascade' }),
+  ...timestamps,
 })
